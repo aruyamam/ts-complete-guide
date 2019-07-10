c813bc0370c94848cd4ec964239e9247f9704d03
aruyamam <aruyamam@gmail.com>
2019-07-10 14:23:09 +0900

    Sorting Arbitrary Collections

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { ChatacterCollection } from './CharacterCollection';
 
-const numberCollection = new NumbersCollection([1000, 3, -5, 0]);
-const sorter = new Sorter(numberCollection);
+// const numberCollection = new NumbersCollection([50, 3, -5, 0]);
+// const sorter = new Sorter(numberCollection);
+// sorter.sort();
+// console.log(numberCollection.data);
+
+const chatacterCollection = new ChatacterCollection('Xaayb');
+const sorter = new Sorter(chatacterCollection);
 sorter.sort();
-console.log(numberCollection.data);
+console.log(chatacterCollection.data);
