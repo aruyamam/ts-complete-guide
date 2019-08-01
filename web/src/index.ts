0b6bb200a9ccfe728500969fae21739e5de1cd0b
aruyamam <aruyamam@gmail.com>
2019-08-01 13:58:46 +0900

    Adding User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hi there!');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.on('change', () => console.log('Change #1'));
+user.on('change', () => console.log('Change #2'));
+user.on('save', () => console.log('Save was triggered'));
+
+user.trigger('fdalfjda');
